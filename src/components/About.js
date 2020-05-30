bdd31414670e29e6fd99258757e097755a05c3d5
Megan Thong <meganthong@outlook.com>
2020-05-30 19:07:11 +0800

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const About = () => {
+    return (
+       <div>
+          <h1>About Us</h1>
+          <h3>Our Mission</h3>
+          <p>We at this website want to connect entry-level job-seekers with each other and with those who have advice and guidance for those who have either recently graduated or recently entered the job market.</p>
+       </div>
+    );
+}
+ 
+export default About;
