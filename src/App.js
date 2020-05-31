47450c47c6a403cb94eb87b51e7fc74f1f7108a9
Megan Thong <meganthong@outlook.com>
2020-05-31 14:06:57 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Navigation from './components/Navigation';
 import LoginForm from './components/Login';
 import SignupForm from './components/Signup';
 import Home from './components/Home';
+import UserAbout from './components/UserAbout';
+import UserAccount from './components/UserAccount';
+import UserBlog from './components/UserBlog';
+import UserForum from './components/UserForum';
+import UserHome from './components/UserHome';
+import UserNavigation from './components/UserNavigation';
 
 import {
   BrowserRouter as Router,
@@ -15,29 +21,28 @@ import {
 
 import AlertComponent from './components/AlertComponent';  
 
+
 function App() {
   const [title , updateTitle] = useState(null);
   const [errorMessage , updateErrorMessage] = useState(null);
     return (      
        <Router>
-        <div className = "App">
+       <div className = "App">
          <header title={title}/>
-          <Navigation />
+          <Navigation/>
             <Switch>
-             <Route path="/" component={Home} exact/>
              <Route path="/Home" component={Home}/>
              <Route path="/About" component={About}/>
              <Route path="/Blog" component={Blog}/>
-             <Route path="/Signup" component={SignupForm}/>
-               <SignupForm showError={updateErrorMessage} updateTitle={updateTitle}/>
-             <Route path="/Login" component={LoginForm}/>
-               <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+             <Route path="/Signup" render={() => <SignupForm showError={updateErrorMessage} updateTitle={updateTitle}/>}/>
+             <Route path="/Login" render={() => <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>}/>
              <Route path="/Home">
                <Home/>
 	     </Route>
+            <Route path="/" component={Home} exact/>
             </Switch>
              <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
-        </div> 
+       </div> 
       </Router>
     );
   }
